664ed5669a9d5795247837892106593341c6b987
Jaspal Gundla <jaspalgundla@gmail.com>
2026-02-16 18:12:25 +0530

    Fix Terraform variable defaults (region and key_name)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = us-east-1
+  default     = "us-east-1"
 }
 
 variable "key_name" {
   description = "EC2 key pair name"
   type        = string
-  default     = jaspal-key
+  default     = "jaspal-key"
 }
 
 variable "instance_type" {
