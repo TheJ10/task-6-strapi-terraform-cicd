450906ee72aafef064172bdb20728ec95f5b2e88
Jaspal Gundla <jaspalgundla@gmail.com>
2026-02-16 18:07:51 +0530

    Add default aws_region for non-interactive Terraform apply

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,13 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
+  default     = us-east-1
 }
 
 variable "key_name" {
   description = "EC2 key pair name"
   type        = string
+  default     = jaspal-key
 }
 
 variable "instance_type" {
